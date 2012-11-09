8549eaa9300086c422787c43e39a6169c4125f88
Tom Janssens <Tom@corebvba.be>
2012-11-09 09:09:38 +0100

    Fixed contract formatting, all tests run now.

--- E014-car-factory-tests/sample-erlang/src/contracts.erl
+++ E014-car-factory-tests/sample-erlang/src/contracts.erl
@@ -2,25 +2,27 @@
 -include("contracts.hrl").
 
 -export([format/1]).
--define(FMT(RecordType,FormatString), 
-	format(Rec=#RecordType{}) -> 
-	  FieldNames=record_info(fields, RecordType),
+-define(FORMAT(RecordType,FormatString), 
+    format(Rec=#RecordType{}) -> 
+      FieldNames=record_info(fields, RecordType),
       Values=tl(tuple_to_list(Rec)),
       PropList=lists:zip(FieldNames,Values),
       subst(FormatString,PropList)
 ).
 
-?FMT(openFactory,"Open Factory(ID='{id}')");
-?FMT(factoryOpened,"Opened Factory(ID='{id}')");
-?FMT(assignEmployeeToFactory,"Assign employee '{employeeName}'");
-?FMT(employeeAssignedToFactory,"new worker joins our forces: '{employeeName}'");
-?FMT(curseWordUttered,"'{theWord}' was heard within the walls. It meant:\r\n    '{meaning}'");
-?FMT(unpackAndInventoryShipmentInCargoBay,"Unload the cargo '{employeeName}'");
-?FMT(produceACar,"Employee '{employee}' produce car:{carModel}");
+?FORMAT(openFactory,"Open Factory(ID='{id}')");
+?FORMAT(factoryOpened,"Opened Factory(ID='{id}')");
+?FORMAT(assignEmployeeToFactory,"Assign employee '{employeeName}'");
+?FORMAT(employeeAssignedToFactory,"new worker joins our forces: '{employeeName}'");
+?FORMAT(curseWordUttered,"'{theWord}' was heard within the walls. It meant:\r\n    '{meaning}'");
+?FORMAT(unpackAndInventoryShipmentInCargoBay,"Unload the cargo '{employeeName}'");
+?FORMAT(produceACar,"Employee '{employeeName}' produce car:{carModel}");
 format(X) -> X.
 
 subst(Fmt,[]) -> 
   Fmt;
+subst(Fmt,[{_,undefined}|T]) ->
+  subst(Fmt,T);
 subst(Fmt,[{K,V}|T]) ->
   Replaced = re:replace(Fmt,"\\{" ++ atom_to_list(K) ++ "\\}",[V],[global,{return,list}]),
     subst(Replaced,T).
