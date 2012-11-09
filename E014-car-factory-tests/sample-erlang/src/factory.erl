cc0d2a3d866ab60d76b0888f6d2d880007b000ec
Tom Janssens <Tom@corebvba.be>
2012-11-09 12:56:11 +0100

    Added factory implementation and specs.

---
+++ E014-car-factory-tests/sample-erlang/src/factory.erl
@@ -0,0 +1,23 @@
+-module(factory).
+-record(factory,{id,employees}).
+-include("contracts.hrl").
+-export([do/2,handle/2]).
+
+
+do(#factory{id=Id},#assignEmployeeToFactory{id=Id,employeeName="Bender"}) ->
+  bender_employee;
+
+do(#factory{id=Id,employees=Employees},#assignEmployeeToFactory{id=Id,employeeName=Name}) ->
+  case lists:member(Name,Employees) of
+    true  -> employee_name_already_taken;
+    false -> #employeeAssignedToFactory{id=Id,employeeName=Name}
+  end;
+
+do(undefined,_) ->
+  factory_is_not_open.
+
+handle(undefined, #factoryOpened{id=Id}) ->
+  #factory{id=Id,employees=[]};
+
+handle(Fact=#factory{id=Id,employees=Employees},#employeeAssignedToFactory{id=Id,employeeName=Name}) ->
+  Fact#factory{employees = [Name|Employees]}.
